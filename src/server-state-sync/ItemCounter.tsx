e38c773ed6c5a6f2dae6ac763cfc8cf6f4f4dc8d
Danijj89 <danijj89@gmail.com>
2022-11-18 08:44:09 +0100

    removes unused code

--- src/server-state-sync/ItemCounter.tsx
+++ src/server-state-sync/ItemCounter.tsx
@@ -1,25 +1,13 @@
-import { QueryCache, useQueryClient } from '@tanstack/react-query';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useTodos } from './queries';
-import { Item } from './types';
 
 export const ItemCounter = () => {
     const { data: todos } = useTodos();
-    const queryClient = useQueryClient();
-
-    const queryCache: QueryCache = queryClient.getQueryCache()
-    const another = queryCache.find<Item[]>(['todos'])
-
-    const [count, setCount] = useState(0);
-
-    useEffect(() => {
-        if (todos?.length) setCount(todos.length);
-    }, [todos?.length])
 
     return (
         <h3>
             <span>Count: </span>
-            <span>{count || 0}</span>
+            <span>{todos?.length || 0}</span>
         </h3>
     );
 }
