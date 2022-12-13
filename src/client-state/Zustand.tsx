1504c1f5b57e8ca366697e68987ce73bac9aefc1
Danijj89 <danijj89@gmail.com>
2022-12-13 09:50:52 -0500

    adds zustand

---
+++ src/client-state/Zustand.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+import { useCurrentUserStore } from './globalStates';
+
+
+export const Zustand = () => {
+    const { setCurrentUser } = useCurrentUserStore();
+
+    useEffect(() => {
+
+    })
+
+    return (
+        <>
+
+        </>
+    );
+}
